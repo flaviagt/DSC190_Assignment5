9579ef6bd92c330e2e45586e8354e92f41118e97
flaviagt <ftedjarutjianta@ucsd.edu>
2026-05-06 21:24:41 -0700

    fixed: resolve lint errors

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import os, sys, json
-
-def main():
-    x = 10
-    print("new 2nd version This line is intentionally made very long to trigger the line-too-long rule. " * 5)
+def main() -> None:
     y = 20
     print(y)
+
 
 if __name__ == "__main__":
     main()
