ab38aaef912e2166866c09b7616d37ad9da30fea
flaviagt <ftedjarutjianta@ucsd.edu>
2026-05-06 21:15:40 -0700

    test 2: intentionally break lint rules

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, sys, json
 
 def main():
     x = 10
-    print("This line is intentionally made very long to trigger the line-too-long rule. " * 5)
+    print("new 2nd version This line is intentionally made very long to trigger the line-too-long rule. " * 5)
     y = 20
     print(y)
 
